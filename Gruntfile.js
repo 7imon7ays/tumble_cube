d8d65dc0d50092f483b8e5e3236742c6c3784b9b
Simon Chaffetz <simis1000@gmail.com>
2015-02-09 21:40:04 -0800

    Change tumble nav to info in Gruntfile, concat css

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
               'dev/js/resize.js',
               'dev/js/tumble_cube_mobile.js',
               'dev/js/tumble_cube_desktop.js',
-              'dev/js/tumble_info.js',
+              'dev/js/tumble_nav.js',
               'dev/js/tumble_cube.js'
             ],
         dest: 'prod/js/<%= pkg.name %>.js'
@@ -26,12 +26,24 @@ module.exports = function(grunt) {
           'prod/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
+    },
+    cssmin: {
+      options: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+          src: 'dev/style.css',
+          dest: 'prod/style/<%= pkg.name %>.min.css'
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-css');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
 
